7382f1a03fadb20d40103568ac6d1692bcf3563a
Ardian Grezda <anigrezda@hotmail.com>
2021-10-07 09:41:37 +0200

    Changed database

--- Algosoft/settings.py
+++ Algosoft/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Algosoft.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project_database',
+        'USER' : 'project_database_user',
+        'PASSWORD' : 'yourpassword',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
