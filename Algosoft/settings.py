b4b759cb2381b1effdcb5913b0b464caa6065dff
ardian <anigrezda@hotmail.com>
2025-10-05 16:54:11 +0200

    Fixed language icons and improved mobile responsiveness for small devices

--- Algosoft/settings.py
+++ Algosoft/settings.py
@@ -51,6 +51,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.template.context_processors.i18n',
+                'django.template.context_processors.static',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
